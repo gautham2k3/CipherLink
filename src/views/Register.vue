dbfd3ac02fcad141572f317911629a0d4b4ae322
bgautham27 <bgautham27@gmail.com>
2025-02-27 07:13:54 +0000

    feat: Enhance account creation confirmation in Register.vue
    - Improved the user experience in the account creation process within Register.vue.
    - Added a visual confirmation with a checkmark icon to indicate successful account registration.
    - Replaced the previous "NEXT" button with a more intuitive checkmark indicator.
    - Updated styling for a cleaner look and feel after registration.
    - Modified the flow such that after registering user sees a checkmark and can then use a next button.

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -30,25 +30,42 @@
    </form>
   </div>
   <div v-else class="right-gard-container">
-    <span style="font-size: 78px; color: #fff;" @click="goToHome" class="fade-in">
+    <!-- <span style="font-size: 78px; color: #fff;" @click="goToHome" class="fade-in">
       <i class="fa-regular fa-circle-check"></i>
     </span>
     <div class="infoContainer">
       <span>Welcome, {{userName}}!</span><br>
       <button class="Home_button" @click="goToHome"><i class="fa-solid fa-circle-arrow-right"></i> NEXT</button>
-    </div>
-    <!-- <div class="grid-item">
+    </div> -->
+    <div class="grid-item">
       <div class="item-style">
         <div class="thumb">
-          <a href="#">
-            <img src="https://thumbor.forbes.com/thumbor/fit-in/x/https://www.forbes.com/advisor/in/wp-content/uploads/2022/03/monkey-g412399084_1280.jpg" alt="">
-          </a>
-          <button class="conformation">
-            <i class="fa-regular fa-circle-check"></i>
-          </button>
+          <span style="font-size: 78px; color: #fff;" @click="goToHome" class="fade-in">
+             <i class="fa-regular fa-circle-check"></i>
+          </span>
+        </div>
+        <div class="nftContent">
+          <h3 class="nftTitle">
+            {{ userName }}
+          </h3>
+          <h5 class="nftUserAddress">
+            {{ walletAddress }}
+          </h5>
+          <div class="nftLastFlex d-flex-between">
+            <a href="#" class="btn">
+              <span>
+                <i class="fa-solid fa-clock-rotate-left"></i>just now
+              </span>
+            </a>
+            <a href="#" @click="goToHome" class="btn btn-cart btn-outline">
+              <span>
+                <i class="fa-solid fa-circle-arrow-right"></i> NEXT
+              </span>
+            </a>
+          </div>
         </div>
       </div>
-    </div> -->
+    </div>
   </div>
   </div>
 </body>
@@ -138,12 +155,20 @@ body, html {
 }
 
 .right-gard-container{
-  width: 400px;
+  width: 320px;
   margin: 0 auto;
+  display: block;
   /* display: flex;
-  flex-wrap: wrap; */
+  flex-wrap: wrap; 
   height:auto;
   border-radius: 20px;
+  background: rgb(153,149,217);
+  background: radial-gradient(circle, rgba(153,149,217,1) 0%, rgba(9,9,121,1) 51%, rgba(2,10,11,1) 100%);
+  */
+}
+.thumb {
+  width: 100%;
+  height: 200px;
   background: rgb(153,149,217);
   background: radial-gradient(circle, rgba(153,149,217,1) 0%, rgba(9,9,121,1) 51%, rgba(2,10,11,1) 100%);
 }
@@ -152,6 +177,18 @@ body, html {
     padding: 0 15px;
     margin-bottom: 30px;
 }
+/* 
+@media (min-width: 768px) {
+    .grid-item {
+        width: 50%;
+    }
+}
+@media (min-width: 992px) {
+    .grid-item {
+        width: 33.33%;
+    }
+} */
+
 .item-style {
     background: #20202d;
     border-radius: 10px;
@@ -162,18 +199,90 @@ body, html {
 }
 .item-style .thumb {
     position: relative;
-    overflow: hidden;
-    display: block;
+    display: flex;
+    justify-content: center;
+    align-items: center;
     border-radius: 10px 10px 0 0;
 }
-.item-style .thumb img {
-    display: block;
-    width: 100%;
-    height: auto;
-    object-fit: cover;
-    transition: 0.5s;
-}
 
+
+.nftContent {
+    padding: 15px 20px;
+}
+.nftTitle {
+    font-size: 18px;
+    padding: 0;
+    margin: 0;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+}
+.nftTitle a {
+    color: #fff;
+}
+.nftUserAddress {
+    font-size: 12px;
+    padding: 0;
+    margin: 0;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+}
+.nftLastFlex {
+  margin-top: 5px;
+  position: relative;
+}
+.d-flex-between {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+}
+.btn span {
+    display: inline-flex;
+    align-items: center;
+    z-index: 1;
+}
+.btn i {
+    padding-right: 8px;
+    transition: 0.1s;
+}
+.btn-cart {
+    position: relative;
+    overflow:hidden ;
+    font-size: 18px;
+    padding: 6px 25px;
+    display: inline-flex;
+    align-items: center;
+    color: #18b625;
+    border-radius: 50px;
+    border: 2px solid rgba(15, 196, 115, 0.7);
+    height: 30px;
+    min-height: 30px;
+    transition: 0.7s;
+}
+.btn-cart:hover {
+    color: #fff;
+    transform: translateY(-2px);
+}
+.btn-outline::before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: linear-gradient(97.3deg, #dd2476 0%, #ff512f 100%);
+    transition: transform 0.7s;
+    transform-origin: right;
+    transform: scaleX(0);
+    z-index: 0;
+    border-radius: inherit;
+}
+.btn-outline:hover::before {
+    transform: scaleX(1);
+    transform-origin: left;
+    transition: transform 0.7s;
+}
 
 .infoContainer {
   display: flex;
